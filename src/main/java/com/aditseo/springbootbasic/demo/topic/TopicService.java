cd3b79e8fae1ad7d5eb0db11d4d1528ffdb17b02
tyadans <yhuri.kyllian@gmail.com>
2020-08-16 16:03:25 +0700

    MVC and CRUD Springboot Basic

---
+++ src/main/java/com/aditseo/springbootbasic/demo/topic/TopicService.java
@@ -0,0 +1,42 @@
+package com.aditseo.springbootbasic.demo.topic;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class TopicService {
+    private List<Topic> topics = new ArrayList<>(Arrays.asList(
+            new Topic("spring", "Spring Framework", "Spring Framework Description"),
+            new Topic("spring2", "Spring Framework2", "Spring Framework Description2"),
+            new Topic("spring3", "Spring Framework3", "Spring Framework Description3"))
+    );
+
+    public List<Topic> getAllTopics(){
+        return topics;
+    }
+
+    public Topic getTopic(String id) {
+        return topics.stream().filter(topic -> topic.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
+    }
+
+    public void updateTopic(String id, Topic topic) {
+        for (int i = 0; i < topics.size(); i++) {
+            Topic t = topics.get(i);
+            if (t.getId().equals(id)) {
+                topics.set(i, topic);
+                return;
+            }
+        }
+    }
+
+    public void deleteTopic(String id) {
+        topics.removeIf(topic -> topic.getId().equals(id));
+    }
+}
